871ccbf892687e5076f26871db1d9663e249ec04
Jeevan Baabu <jeevanbaabu03@gmail.com>
2025-06-06 01:37:23 +0400

    updated log

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -4,8 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
   }
 })
